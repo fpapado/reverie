34c773c171ec4d97f3e62b97f04a3713f3903c59
fpapado <fgpapado@gmail.com>
2016-12-05 18:33:46 +0000

    Revert changes to imports

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-import {Ember, Application} from 'ember';
+import Application from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
@@ -7,7 +7,7 @@ let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Application.extend({
+App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
