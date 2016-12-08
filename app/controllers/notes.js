538ba39c877c8425469ba7cc84f78075933e1c1c
fpapado <fgpapado@gmail.com>
2016-12-08 23:26:03 +0000

    Redirect to note list after creating note

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -5,18 +5,21 @@ const { Controller } = Ember;
 export default Controller.extend({
   actions: {
     createNote() {
-      let inspection = this.get('store').createRecord('note', {
+      let note = this.get('store').createRecord('note', {
         title: this.get('note-title'),
         author: this.get('note-author'),
         category: this.get('note-category'),
         content: this.get('note-content')
       });
-      inspection.save();
+      note.save().then(
+        this.transitionToRoute('notes'),
+        () => { }
+      );
 
-      this.set('note-title', '');
-      this.set('note-author', '');
-      this.set('note-category', '');
-      this.set('note-content', '');
+      // this.set('note-title', '');
+      // this.set('note-author', '');
+      // this.set('note-category', '');
+      // this.set('note-content', '');
     }
   }
 });
