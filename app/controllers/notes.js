e654b0af3b30e97a868535709e913ee7f3890754
fpapado <fgpapado@gmail.com>
2016-12-13 13:45:26 +0000

    Make async more robust with ember-concurrency

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,25 +1,24 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
 const { Controller } = Ember;
 
 export default Controller.extend({
-  actions: {
-    createNote() {
-      let note = this.get('store').createRecord('note', {
-        title: this.get('note-title'),
-        author: this.get('note-author'),
-        category: this.get('note-category'),
-        content: this.get('note-content')
-      });
-      note.save().then(
-        this.transitionToRoute('notes'),
-        () => { }
-      );
+  createNote: task(function* () {
+    let note = this.get('store').createRecord('note', {
+      title: this.get('note-title'),
+      author: this.get('note-author'),
+      category: this.get('note-category'),
+      content: this.get('note-content')
+    });
 
-      this.set('note-title', '');
-      this.set('note-author', '');
-      this.set('note-category', '');
-      this.set('note-content', '');
-    }
-  }
+    yield note.save();
+
+    this.set('note-title', '');
+    this.set('note-author', '');
+    this.set('note-category', '');
+    this.set('note-content', '');
+
+    this.transitionToRoute('notes');
+  }).drop()
 });
