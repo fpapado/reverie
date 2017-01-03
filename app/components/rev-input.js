f65356098e9c566561a9dcd3e92f2a52b1345c4c
fpapado <fgpapado@gmail.com>
2017-01-03 23:58:38 +0200

    Further style rev-card and rev-input components

--- app/components/rev-input.js
+++ app/components/rev-input.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const { Component, computed } = Ember;
 
 export default Component.extend({
-  classNames: ['input-field'],
+  classNames: ['mb4', 'input-field'],
   type: 'text',
   _errorMessages: computed('errors.[]', function() {
     return (this.get('errors') || []).join(', ');
