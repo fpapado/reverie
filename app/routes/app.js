c22ea6528ba6746635b9a87bfd362bc4cf4f6d3f
fpapado <fgpapado@gmail.com>
2017-01-04 04:09:53 +0200

    Fetch current user from token API, add to session
    Our API backend does not return a user object upon authenticating
    rather a JWT token, per OAuth. In order to get the user object
    from it we have to hit the /api/user/current endpoint, which reads the
    user from the token and returns them formatted as JSONAPI.
    We use ember-network fetch, because it is Fastboot-friendly.

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import config from '../config/environment';
+import fetch from 'ember-network/fetch';
 
 const { Route, inject } = Ember;
 
@@ -9,5 +11,23 @@ export default Route.extend({
     if (!this.get('session').get('isAuthenticated')) {
       this.transitionTo('auth.login');
     }
+  },
+  // Set session.currentUser:
+  // Our API backend does not return a user object upon authenticating
+  // rather a JWT token, per OAuth. In order to get the user object from it
+  // we have to hit the /api/user/current endpoint, which reads the user from
+  // the token and returns them formatted as JSONAPI.
+  afterModel() {
+    return fetch(`${config.DS.host}/${config.DS.namespace}/user/current`, {
+      type: 'GET',
+      headers: {
+        'Authorization': `Bearer ${this.get('session').get('session.content.authenticated.access_token')}`
+      }
+    }).then((raw) => {
+      return raw.json().then((data) => {
+        let currentUser = this.store.pus(data);
+        this.set('session.currentUser', currentUser);
+      });
+    });
   }
 });
