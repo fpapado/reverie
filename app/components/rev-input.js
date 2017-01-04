f974147f1f0a04e796129b6336316c9643f66671
fpapado <fgpapado@gmail.com>
2017-01-04 02:17:55 +0200

    Polish error state style and message in validation

--- app/components/rev-input.js
+++ app/components/rev-input.js
@@ -5,6 +5,7 @@ const { Component, computed } = Ember;
 export default Component.extend({
   classNames: ['mb4', 'input-field'],
   type: 'text',
+  // Concatenate all validation errors into comma-separated string
   _errorMessages: computed('errors.[]', function() {
     return (this.get('errors') || []).join(', ');
   })
