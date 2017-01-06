b2b25a9b9ea90b0a60f7418f51fd6fe93381072f
fpapado <fgpapado@gmail.com>
2017-01-07 00:39:33 +0200

    Add ember-concurrency task on route, not good
    While the task is performed as a closure action, it loses
    its magical properties. Thus, we are back to declaring it on a
    controller and passing the closure in the templete. In the
    future, it might be worth it to implement a route-task helper.

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
 const { Route, inject } = Ember;
 
@@ -8,29 +9,42 @@ export default Route.extend({
 
   actions: {
     doLogin() {
-      let user = this.get('currentModel');
-      this.get('session')
-        .authenticate(
-          'authenticator:reverie', user.email, user.password
-        ).then(() => {
-          // Success
-          this.get('notify').success('Logged In!');
-        }).catch((response) => {
-          let { errors } = response;
-
-          // Check if there's a 401
-          if (errors.mapBy('code').indexOf(401) >= 0) {
-            // Flash Unauthorized
-            this.get('notify')
-              .error('Incorrect username or password, please try again');
-          } else {
-            // Other API errors
-            this.get('notify')
-              .error('There was a Server Error, please try again later');
-          }
-        });
+      return this.get('doLoginTask').perform();
     }
   },
+
+  doLoginTask: task(function* () {
+    // change to this.get(this.routeName)
+    let user = this.get('currentModel');
+
+    timeout(5000);
+
+    try {
+      yield this.get('session')
+        .authenticate(
+            'authenticator:reverie',
+            user.email,
+            user.password
+            )
+        // Success
+        this.get('notify').success('Logged In!');
+
+    } catch(e) {
+      let { errors } = e;
+
+      // Check if there's a 401
+      if (errors.mapBy('code').indexOf(401) >= 0) {
+        // Flash Unauthorized
+        this.get('notify')
+          .error('Incorrect username or password, please try again');
+      } else {
+        // Other API errors
+        this.get('notify')
+          .error('There was a Server Error, please try again later');
+      }
+    }
+  }).drop(),
+
   model() {
     return {
       email: '',
