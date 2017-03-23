5b63337c9aba9d04721521cb9ef81af4ff588850
fpapado <fgpapado@gmail.com>
2017-03-23 21:58:12 +0000

    Change user sticker query to use session user
    This change allows better linkage and caching in the model, but
    is implicit in two ways: 1. sideloading is handled in the API
    instead of specifying in query, 2. Ember-data handles the linkage
    via the object model. These are adequate tradeoffs, but if the API
    changes, we might want to reconsider.

--- app/routes/app/stickers/index.js
+++ app/routes/app/stickers/index.js
@@ -6,15 +6,25 @@ export default Route.extend({
   session: inject.service(),
 
   model() {
-    let uid = this.get('session').currentUser.id;
+    // Used with the explicit query below, if enabled
+    // let uid = this.get('session').currentUser.id;
     return RSVP.hash({
       // A downside of this approach is that the data is not linked to the
-      // user's model.
-      // TODO: Investigate if we can sideload 'sender' if we fetch from
-      //  the user relationship instead
-      // TODO: Also consider reload: true or background reload
+      // user's model, but makes sideloading explicit.
       // eslint-disable-next-line camelcase
-      stickers: this.store.query('sticker', {user_id: uid, include: 'sender'}),
+      // stickers: this.store.query('sticker', {user_id: uid, include: 'sender,categories'}),
+
+      // A bit "magical", as Ember-data handles loading the async relationship
+      // via GET at /users/:id/stickers. Note that this method does not allow
+      // for explicit sideloading atm. The API currently always sideloads
+      // categories and senders, but if it changes, use the query above.
+
+      // The upside is that this allows better linking/caching of a user's
+      // stickers in the model, which does not happen with store.query.
+      stickers: this.get('session').currentUser.get('stickers', {include: 'sender'}),
+
+      // It is important to have the categories available, even if the user
+      // has not been awarded all of them (thus not included in query above)
       categories: this.store.findAll('category')
     });
   }
